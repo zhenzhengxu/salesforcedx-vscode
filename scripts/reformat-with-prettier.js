c5db45293aadf87c61881bcf1acce767cf45affe
Luis Campos <lcamposguajardo@salesforce.com>
2018-07-12 10:50:40 -0700

    prettier config fixes

--- scripts/reformat-with-prettier.js
+++ scripts/reformat-with-prettier.js
@@ -3,6 +3,9 @@
 const path = require('path');
 const shell = require('shelljs');
 
+console.log('**********************');
+console.log('reformat with prettier');
+console.log('**********************');
 const prettierExecutable = path.join(
   __dirname,
   '..',
@@ -11,9 +14,16 @@ const prettierExecutable = path.join(
   'prettier'
 );
 
-shell.exec(
-  `${prettierExecutable} --single-quote --write "packages/salesforcedx-*/package.json" "package.json"`,
+console.log('**********************');
+console.log('prettier executable = ', prettierExecutable);
+console.log('**********************');
+const log = shell.exec(
+  `${prettierExecutable} --write --config .prettierrc.json 'package.json'`,
   {
     cwd: path.join(__dirname, '..')
   }
-);
+).stdout;
+
+console.log('**********************');
+console.log('prettier exec log = ', log);
+console.log('**********************');
