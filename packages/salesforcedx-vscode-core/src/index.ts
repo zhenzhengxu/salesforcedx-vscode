5051930af69fd05aa17977f268292455ca376ce4
Justin Kalloor <jkalloor@jkalloor-ltm.internal.salesforce.com>
2018-07-19 13:28:46 -0600

    Merging Merging Merging

--- packages/salesforcedx-vscode-core/src/index.ts
+++ packages/salesforcedx-vscode-core/src/index.ts
@@ -414,7 +414,6 @@ export async function activate(context: vscode.ExtensionContext) {
   const testProvider = vscode.window.registerTreeDataProvider('sfdx.force.test.view', testOutlineProvider);
   context.subscriptions.push(testProvider);
 
-<<<<<<< HEAD
   // Run Test Button on Test View command
   vscode.commands.registerCommand('sfdx.force.test.view.run', () => testOutlineProvider.runApexTests());
   // Show Error Message command
@@ -430,11 +429,6 @@ export async function activate(context: vscode.ExtensionContext) {
   vscode.window.onDidChangeTextEditorSelection(() => {
     testOutlineProvider.clearDecorations();
   });
-=======
-  // Run Test Button on Test View
-  vscode.commands.registerCommand('sfdx.force.test.view.run', () => testOutlineProvider.runApexTests());
-
->>>>>>> Initial save for testOutlineCode
   // Task View
   const treeDataProvider = vscode.window.registerTreeDataProvider(
     'sfdx.force.tasks.view',
