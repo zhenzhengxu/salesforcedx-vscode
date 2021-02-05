2b2e46d4a423d51bc942e4914f7c69a90b290983
Xiaoyi Chen <xchen@salesforce.com>
2021-02-04 17:19:22 -0800

    refactor: use node-prune

---
+++ scripts/install-node-prune.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -sf https://gobinaries.com/tj/node-prune | sh
+
--- scripts/trim-node-modules.js
+++
@@ -1,7 +0,0 @@
-const rimraf = require('rimraf');
-
-rimraf('packages/salesforcedx-*/node_modules/jsforce/build', function(error) {
-  if (error) {
-    throw error;
-  }
-});
